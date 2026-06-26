6518cb820c2a6347bc15cd4500a6e49d80d1e188
roidrole <83431927+roidrole@users.noreply.github.com>
2026-06-26 19:37:59 -0400

    Readd disallowEmptyStacks and disallowComplexEmptyStacks
    Crashes on load when only fasterHash is enabled on Tainted Forces

--- src/main/java/roidrole/thaumicroid/mixins/ThaumcraftAPIUseHash.java
+++ src/main/java/roidrole/thaumicroid/mixins/ThaumcraftAPIUseHash.java
@@ -4,7 +4,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.internal.CommonInternals;
 
 @Mixin(ThaumcraftApi.class)
@@ -37,5 +41,29 @@ public abstract class ThaumcraftAPIUseHash {
 		}
 		item.setItemDamage(oldDamage);
 		return false;
+	}
+
+	@Inject(
+		method = "registerObjectTag(Lnet/minecraft/item/ItemStack;Lthaumcraft/api/aspects/AspectList;)V",
+		at = @At("HEAD"),
+		cancellable = true,
+		remap = false
+	)
+	private static void disallowEmptyStacks(ItemStack item, AspectList aspects, CallbackInfo ci) {
+		if (item == null || item.isEmpty()) {
+			ci.cancel();
+		}
+	}
+
+	@Inject(
+		method = "registerComplexObjectTag(Lnet/minecraft/item/ItemStack;Lthaumcraft/api/aspects/AspectList;)V",
+		at = @At("HEAD"),
+		cancellable = true,
+		remap = false
+	)
+	private static void disallowComplexEmptyStacks(ItemStack item, AspectList aspects, CallbackInfo ci) {
+		if (item == null || item.isEmpty()) {
+			ci.cancel();
+		}
 	}
 }
